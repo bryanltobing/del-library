af30849f0d56a67007538deafbad2dc43b2a23db
bryantobing12 <bryantobing0@gmail.com>
2020-06-16 23:51:38 +0700

    add cardrequest model

---
+++ models/card_request.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const cardSchema = mongoose.Schema({
+    fullname : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    email : {
+        type : String,
+        trim : true,
+        required : true,
+        unique : true
+    },
+    prodi : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    fakultas : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    nomorinduk : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    phoneNumber : {
+        type : Number,
+        trim : true,
+        required : true
+    },
+    message : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    isApproved : {
+        type : Boolean,
+        default : false
+    }
+
+});
+
+
+
+const Card = mongoose.model('cards', cardSchema);
+
+module.exports = Card;
