525d8f6e2aaedbf8b918a2f77afd3ebfa57375b9
bryantobing12 <bryantobing0@gmail.com>
2020-06-25 23:59:32 +0700

    add book models

---
+++ models/books.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    isbn : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    judul : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    bahasa : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    pengarang : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    tahun : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    penerbit : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    lokasi : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    deskripsi : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    gambar : {
+        type : Buffer,
+    }
+});
+
+
+const Books = mongoose.model('books', bookSchema);
+
+module.exports = Books;
