e8fff8720dca07778aaf4e0f2922559dcd10d48e
bryantobing12 <bryantobing0@gmail.com>
2020-06-30 17:45:38 +0700

    add index display

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ const router = express.Router();
 
 // Models
 const Book = require('../models/books');
+const CD = require('../models/cd_dvd');
+const Article = require('../models/article');
 
 router.get('/', async function(req, res) {
     const book = await Book.countDocuments();
+    const cd = await CD.countDocuments();
+    const article = await Article.countDocuments();
     res.render('pages/index' , {
         title : "OLIS ITDEL",
-        data : Number(book)
+        data : {
+            book : Number(book),
+            cd : Number(cd),
+            article : Number(article)
+        }
     });
 });
 
