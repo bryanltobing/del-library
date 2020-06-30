e500526ba55623c5813e63961b9cdbed848aff68
bryantobing12 <bryantobing0@gmail.com>
2020-07-01 01:16:03 +0700

    make route for pengumuman list

--- routes/pengumumanAll.js
+++ routes/pengumumanAll.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// models
+const Pengumuman = require('../models/pengumuman');
 
+router.get('/pengumuman-list', async (req, res) => {
+    try {
+        const pengumuman = await Pengumuman.find({});
+        res.send(pengumuman);
+    } catch(e) {
+        console.log("error " + e);
+    }
+});
 
 
 module.exports = router;
