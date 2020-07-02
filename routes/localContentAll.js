6da3ab721e6538daa8f81e13b5b11432a90ea59b
bryantobing12 <bryantobing0@gmail.com>
2020-07-03 01:12:39 +0700

    minor fix display and add form to book list

--- routes/localContentAll.js
+++ routes/localContentAll.js
@@ -5,15 +5,22 @@ const LocalContent = require('../models/local_content');
 
 router.get('/localcontent-list', async (req, res) => {
     try {
-        const localcontent = await LocalContent.find({});
+        var limit = parseInt(req.query.limit);
+        if(!limit){
+            limit=7;
+        }
+        const localcontent = await LocalContent.find({}).limit(limit);
+        const count = await LocalContent.countDocuments();
         res.render('pages/localcontent_list', {
             title : "Local Content",
             data : localcontent,
+            count,
+            limit,
             localContentSuccess : req.flash('localContentSuccess')
         });
     } catch(e) {
         res.redirect('/');
     }
-})
+});
 
 module.exports = router;
