153c1d2826fe04905d1a3bb5647cea367bb3d65e
bryantobing12 <bryantobing0@gmail.com>
2020-07-03 18:21:12 +0700

    add pengaturan menu

--- routes/pengaturan.js
+++ routes/pengaturan.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('pengaturan')
+    res.render('pages/pengaturan', {
+        title : "Pengaturan"
+    })
 }); 
 
 
