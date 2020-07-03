29e19f4085f138c341d1fcad863e08b68b61df1b
bryantobing12 <bryantobing0@gmail.com>
2020-07-03 13:19:30 +0700

    search feature for cd/dvd

--- routes/cd_dvdAll.js
+++ routes/cd_dvdAll.js
@@ -2,13 +2,23 @@ const express = require('express');
 const router = express.Router();
 const { auth, notAuth, authRoleLibrarian } = require('../middleware/auth');
 const moment = require('moment-timezone');
+const escapeRegex = require('../function/search');
 
 const CD = require('../models/cd_dvd');
 
 
 router.get('/cd_dvd-list', async (req, res) => {
     try {
-        const cd = await CD.find({});
+        if(!req.query.keywords) {
+            const cd = await CD.find({});
+            return res.render('pages/cd_dvd', {
+                title : "CD/DVD",
+                data : cd,
+                cdSuccess : req.flash('cdSuccess')
+            });
+        } 
+        const regex = new RegExp(escapeRegex(req.query.keywords), 'gi');
+        const cd = await CD.find({ $or : [ { judul : regex }, { sumber : regex } ] });
         res.render('pages/cd_dvd', {
             title : "CD/DVD",
             data : cd,
