9e7a7c89565898379ac149d494c245a768bcb7e3
bryantobing12 <bryantobing0@gmail.com>
2020-07-17 00:52:11 +0700

    Fix Collide Configuration for dev and prod

--- config/db/mongoose.js
+++ config/db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
 mongoose.connect(
     process.env.MONGODB_URL,
